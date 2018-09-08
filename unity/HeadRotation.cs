cd13bde493dd10e35eccdf0182ae14dffec97af2
Philipp Weitz <philipp.weitz@futurice.com>
2018-09-09 02:15:05 +0300

    add unit scripts

---
+++ unity/HeadRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+
+[System.Serializable]
+public class HeadRotation
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public static HeadRotation fromVector3(Vector3 vector) {
+        HeadRotation rotation = new HeadRotation();
+        rotation.x = vector.x;
+        rotation.y = vector.y;
+        rotation.z = vector.z;
+
+        return rotation;
+    }
+}
---
+++ unity/SendHeadRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class SendHeadRotation : MonoBehaviour
+{
+    private WebSocket ws;
+
+    IEnumerator Start()
+    {
+        ws = new WebSocket(new Uri("add-ws-server-url-here"));
+        yield return StartCoroutine(ws.Connect());
+
+        InvokeRepeating("SendHeadRotationTask", 1.0f, 0.1f);
+    }
+
+    void SendHeadRotationTask()
+    {
+        // get camera rotation
+        Vector3 rotation = Camera.main.gameObject.transform.rotation.eulerAngles;
+
+        string json = JsonUtility.ToJson(HeadRotation.fromVector3(rotation));
+        ws.SendString(json);
+
+        if (ws.error != null)
+        {
+            Debug.LogError("Error connecting to web socket server");
+        }
+    }
+}
